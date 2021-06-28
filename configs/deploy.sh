7ae93d7d23dde96ca637aecd9a87ed5059600e3c
nick <me@nickroy.org>
2021-06-28 14:09:48 -0400

    updated

--- configs/deploy.sh
+++ configs/deploy.sh
@@ -1,4 +1,9 @@
 mkdir ./configs
-wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/pods.yaml
-wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/services.yaml
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/pods.yaml -P ./configs
+wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/services.yaml -P ./configs
 kubectl apply -f configs/
