639ac0d58ed1b7477b605371a455bfcb8c130ec6
nick <me@nickroy.org>
2021-06-28 13:32:54 -0400

    deploy script

---
+++ configs/deploy.sh
@@ -0,0 +1,4 @@
+mkdir ./configs
+wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/pod$
+wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/ser$
+kubectl apply -f configs/
