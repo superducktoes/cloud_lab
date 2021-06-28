2a7019f6174f321700b2e58dd17015eec0ffdbcb
nick <me@nickroy.org>
2021-06-28 14:11:39 -0400

    updated

--- configs/deploy.sh
+++ configs/deploy.sh
@@ -3,6 +3,7 @@ mkdir ./configs
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/pods.yaml -P ./configs
 wget https://raw.githubusercontent.com/superducktoes/cloud_lab/main/configs/services.yaml -P ./configs
